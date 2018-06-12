a394398d1f0372daac145520c0685ff3a33008a8
Luan Nguyen <nguyen.n.luan777@gmail.com>
2018-06-12 12:25:45 +0200

    Added example data, scripts and output

---
+++ example/scripts/RF_predict.R
@@ -0,0 +1,10 @@
+library(mltoolkit)
+
+base_dir <- '/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_PCAGW/scripts/mltoolkit/example/'
+model_out_path <- paste0(base_dir,'/','data/RF_BRCA_EU.rds')
+
+rf_model <- readRDS(model_out_path)
+
+predict(object = rf_model, 
+        newdata = m, 
+        type = "prob")
