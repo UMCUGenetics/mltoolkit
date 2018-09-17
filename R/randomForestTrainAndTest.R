4e8f7143b05bb565aac0b39e7d69d657b16a7ecd
Luan Nguyen <nguyen.n.luan777@gmail.com>
2018-09-17 17:35:12 +0200

    Enabled multiclass balancing via balance.options (points to balanceClassesMulti.R). Enabled the reassignment of positive/negative classes after balancing.

--- R/randomForestTrainAndTest.R
+++ R/randomForestTrainAndTest.R
@@ -9,9 +9,17 @@
 #' @param test Test dataset. If test is NULL, no prediction will be performed. A dataframe of observations as rows and features 
 #' as columns. \strong{Important:} input matrix/dataframe should include the response vector as a column.
 #' @param colname.response The name of a column containing the response classes.
-#' @param balance Balancing of classes by simple up/down sampling.
 #' @param incl.expected.response Whether to include the response vector as a column in the dataframe containing the
 #' prediction probabilities. This must be TRUE for the downstream statistics plots to work.
+#' 
+#' @param balance.options A list of vectors stating the options of the base function, balanceClasses(). Vectors should be in the 
+#' format: c(target.class, scale.ratio, method)
+#' 
+#' @param p.responses Convert a multiclass problem to a binary problem. Positive and/or negative classes can be targeted with
+#' 'p.responses'/'n.responses' (a character vector) and converted to as indicated by 'p.return'/'n.return'.
+#' @param n.responses See 'p.responses'
+#' @param p.return See 'p.responses'
+#' @param n.return See 'p.responses'
 #'
 #' @param ntreeTry From \pkg{randomForest::tuneRF()}. Number of decision trees to create when determining the optimal mtry
 #' value.
@@ -36,7 +44,13 @@
 #' @export
 #'
 
-randomForestTrainAndTest <- function(train, test = NULL, colname.response, balance = F, incl.expected.response = T,
+randomForestTrainAndTest <- function(train, test = NULL, colname.response, incl.expected.response = T,
+                                     
+                                     balance.options = NULL,
+                                     
+                                     ## reassign classes
+                                     p.responses = NULL, p.return = NULL,
+                                     n.responses = NULL, n.return = NULL,
 
                                      ## randomForest::tuneRF() args
                                      ntreeTry = 500, stepFactor = 1.2, improve = 0.001, plot = F, trace = F, equal.oobe.decision = 'max',
@@ -47,17 +61,27 @@ randomForestTrainAndTest <- function(train, test = NULL, colname.response, balan
                                      ## Other
                                      ...)
 {
-   ## Up/down balance classes
-   if(balance == T | balance == 'up'){
-      train <- balanceClasses(train, colname.response, scaling = 'up')
-   } else if (balance == 'down'){
-      train <- balanceClasses(train, colname.response, scaling = 'down')
+   if(!is.null(balance.options)){
+      train <- balanceClassesMulti(train, colname.response = colname.response, balance.options = balance.options)
+   }
+   
+   ## reassign classes
+   if( !is.null(p.responses) & !is.null(p.return) ){
+      train[,colname.response] <- toBinaryResponse(train[,colname.response],
+                                                   p.responses = p.responses,
+                                                   p.return = p.return)
+   }
+   
+   if( !is.null(n.responses) & !is.null(n.return) ){
+      train[,colname.response] <- toBinaryResponse(train[,colname.response], 
+                                                   n.responses = n.responses,
+                                                   n.return = n.return)
    }
 
    ## Get mtry where OOBE is min
    if( is.null(mtry) ){
-      mtryTune <- tuneRF(x = train %>% .[,colnames(.) != colname.response], #df of features/observations
-                         y = train %>% .[,colname.response], ## vector of expected response
+      mtryTune <- tuneRF(x = train[,colnames(train) != colname.response], #df of features/observations
+                         y = train[,colname.response], ## vector of expected response
                          ntreeTry=ntreeTry,
                          stepFactor=stepFactor,
                          improve=improve,
@@ -65,9 +89,7 @@ randomForestTrainAndTest <- function(train, test = NULL, colname.response, balan
                          trace=trace,
                          ...)
 
-      mtry <-
-         mtryTune %>%
-         .[.[,2] == min(.[,2]),1]
+      mtry <- mtryTune[mtryTune[,2] == min(mtryTune[,2]),1]
 
       if(equal.oobe.decision == 'max'){
          mtry <- mtry[length(mtry)]
@@ -77,8 +99,8 @@ randomForestTrainAndTest <- function(train, test = NULL, colname.response, balan
    }
 
    ## Fit RF model
-   RF <- randomForest(x = train %>% .[,colnames(.) != colname.response],
-                      y = train %>% .[,colname.response],
+   RF <- randomForest(x = train[,colnames(train) != colname.response],
+                      y = train[,colname.response],
                       ntree = randomForest.ntree,
                       importance = importance,
                       mtry = mtry,
@@ -91,7 +113,7 @@ randomForestTrainAndTest <- function(train, test = NULL, colname.response, balan
    if(!is.null(test)){
       ## Predict on test set
       pred <- predict(object = RF, newdata = test[,colnames(test) != 'response'], type = "prob")
-      pred <- pred %>% as.data.frame()
+      pred <- as.data.frame(pred)
       
       if(incl.expected.response == T){
          pred$response <- test[,colname.response]
