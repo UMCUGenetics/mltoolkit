17451a66766944668ccfb1b8b512c330c99494da
Luan Nguyen <nguyen.n.luan777@gmail.com>
2018-09-17 17:11:31 +0200

    Up/downsampling methods changed to 'simple' and 'SMOTE'. Enabled more granular scaling of multiple classes via balanceClassesMulti.R

--- R/balanceClasses.R
+++ R/balanceClasses.R
@@ -1,46 +1,67 @@
 #' Balancing of classes by up/down sampling
 #'
-#' @description Data has class imbalance if e.g. there are 100 BRCA deficient donors and 900 BRCA proficient donors.
-#' If scaling = 'up' the 100 BRCA deficient donors are up-sampled to 900. If scaling = 'down' the 900 BRCA deficient donors
-#' are down-sampled to 100. This function also works if there are multiple classes (i.e. response is not binary)
+#' @description Data has class imbalance if e.g. there are 100 BRCA deficient donors and 900 BRCA proficient donors. This 
+#' function up/down-samples a desired class.
 #'
-#' @param df A dataframe of observations as rows and features as columns
-#' @param colnameResponse The name of a column containing the response classes
-#' @param scaling If scaling = 'up', less represented classes are sampled up (with replacement) to the highest represented class.
-#' If scaling = 'down', higher represented classes are sampled down to the lowest represented class.
+#' @param df A dataframe of samples as rows and features as columns
+#' @param colname.response The name of a column containing the response classes
+#' @param target.class The target class for which the sample size will be adjusted
+#' @param scale.ratio The amount of up/down-sampling (e.g. 2 is 2x up, 0.5 is 2x down)
+#' @param method 'simple': random samples are replicated when upsampling or random samples are removed when downsampling.
+#' 'SMOTE': synthesizes samples based on the feature values of a class (can only be used for upsampling)
 #'
 #' @return Returns a dataframe with balanced classes
 #' @export
 #'
-#' @examples balanceClasses(df, 'response', scaling = 'up')
+#' @examples balanceClasses(df, 'response', 'BRCA1', 1.5, 'SMOTE')
 
-balanceClasses <- function(df, colnameResponse, scaling = 'up')
-{
-   classFreq <- df[,colnameResponse] %>% table() %>% as.data.frame()
+balanceClasses <- function(df, colname.response, target.class, scale.ratio, method = 'simple'){
 
-   if(scaling == 'up'){
-      targetFreq <- classFreq$Freq %>% max()
-   } else if(scaling == 'down') {
-      targetFreq <- classFreq$Freq %>% min()
+   scale.ratio <- as.numeric(scale.ratio)
+   
+   df_split <- list(
+      target = df[ df[, colname.response] == target.class,],
+      remainder = df[ df[, colname.response] != target.class,]
+   )
+   
+   if(scale.ratio == 1){
+      stop('scale.ratio must not == 1')
    }
 
-   targetFreqClass <- classFreq[classFreq$Freq == targetFreq,'.']
+   targetClassFreq <- nrow(df_split$target)
+   newTargetClassFreq <- round(targetClassFreq * scale.ratio)
+      
+   if(method == 'simple'){
 
-   df_BalancedClasses <- lapply( as.character(classFreq$.), function(i){
-      df_ss <- df[df[,colnameResponse] == i,]
-
-      if(scaling == 'up'){
-         if(i != targetFreqClass){
-            sample(1:nrow(df_ss), targetFreq, replace = T) %>% df_ss[.,]
-         } else {
-            df_ss
-         }
-
-      } else if(scaling == 'down'){
-         sample(1:nrow(df_ss), targetFreq, replace = F) %>% df_ss[.,]
+      if(scale.ratio > 1){
+         sample_with_replace <- T
+      } else {
+         sample_with_replace <- F
       }
-
-   }) %>% do.call(rbind,.) %>% .[order(rownames(.)),]
-
-   return(df_BalancedClasses)
+      
+      df_split$target <- 
+         sample(1:targetClassFreq, 
+                newTargetClassFreq, 
+                replace = sample_with_replace) %>% df_split$target[.,]
+   }
+   
+   if(method == 'SMOTE'){
+      if(scale.ratio <= 1){
+         stop('For method = \'SMOTE\', scale.ratio must be > 1')
+      } else {
+         scale.ratio <- (scale.ratio - 1) * 100
+      }
+      
+      df_dummy <- df_split$remainder[1,]
+      df_dummy <- df_dummy[rep(1,targetClassFreq + 1),]
+      
+      df_SMOTE <- rbind(df_split$target,df_dummy)
+      df_SMOTE[,colname.response] <- as.factor(df_SMOTE[,colname.response])
+      
+      df_split$target <- SMOTE(response ~ ., df_SMOTE, perc.over = 100, perc.under = 0)
+   }
+   
+   return(
+      rbind(df_split$target, df_split$remainder)
+   )
 }
