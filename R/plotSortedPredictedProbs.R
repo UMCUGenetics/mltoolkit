06945f977e8bece1dd829ee930b6a6a5495b56da
Luan Nguyen <nguyen.n.luan777@gmail.com>
2019-04-04 17:36:57 +0200

    Fixed issue where the legend key color mismatched with the legend text

--- R/plotSortedPredictedProbs.R
+++ R/plotSortedPredictedProbs.R
@@ -11,16 +11,25 @@
 #' @export
 #'
 
-plotSortedPredictedProbs <- function(probs.predicted, logicals.expected = NULL,
-                                     cutoff = NULL, show.confusion = T, annotations = NULL, title = NULL)
+plotSortedPredictedProbs <- function(probs.predicted, logicals.expected = NULL, cutoff = NULL, 
+                                     show.confusion = T, show.hline = T, annotations = NULL, title = NULL)
 {
-   if( is.null(logicals.expected) ){
-      df <- data.frame(probs.predicted)
-   } else {
-      df <- data.frame(probs.predicted, logicals.expected)
-   }
    
-   df <- df$probs.predicted %>% order(.) %>% df[.,]
+   # probs.predicted = hmf_prediction_sum_brca12_ss_truth_set$BRCA
+   # logicals.expected = hmf_prediction_sum_brca12_ss_truth_set$response
+   
+   if( is.null(logicals.expected) ){
+      df <- data.frame(probs.predicted = sort(probs.predicted))
+   } else {
+      ## Making sure that logicals.expected is in the right format. Factor ordering affects ggplot bar fill color
+      logicals.expected <- replace(logicals.expected, logicals.expected %in% c('TRUE',T), 1)
+      logicals.expected <- replace(logicals.expected, logicals.expected %in% c('FALSE',F), 0)
+      logicals.expected <- factor(logicals.expected, levels = c('0','1'))
+      
+      df <- data.frame(probs.predicted, logicals.expected)
+      df <- df[order(df$probs.predicted),]
+   }
+
    df$index <- 1:nrow(df)
    
    plot <- ggplot(data = df, aes(x = index, y = probs.predicted) ) +
@@ -28,47 +37,56 @@ plotSortedPredictedProbs <- function(probs.predicted, logicals.expected = NULL,
       theme(plot.title = element_text(hjust = 0.5),
             panel.background = element_blank(),
             panel.grid = element_blank(),
-            axis.line = element_line(colour = "black"))
+            axis.line = element_line(colour = "black"),
+            legend.title=element_blank())
    
    if( !is.null(logicals.expected) ){
       plot <- plot + geom_bar(stat = 'identity', width = 1, aes(fill = logicals.expected ) )
          
-      if(!is.null(annotations)){
-         plot <- plot + scale_fill_manual(name = 'Annotation', labels = annotations, values = c('red','grey'))
+      if(is.null(annotations)){
+         plot <- plot + scale_fill_manual(values = c('grey','red'))
+      } else {
+         plot <- plot + scale_fill_manual(labels = annotations, values = c('grey','red'))
       } 
-      
-      else {
-         plot <- plot + scale_fill_manual(name = 'Annotation', values = c('red','grey'))
-      }
-      
+
    } else {
-      plot <- plot + geom_bar(stat = 'identity', width = 1)
+      plot <- plot + geom_bar(stat = 'identity', width = 1, color = 'grey')
    }
    
    if( !is.null(title) ){ plot <- plot + ggtitle(title) }
-   if( !is.null(cutoff) ){ plot <- plot + geom_hline(yintercept = cutoff, linetype = 2) }
    
    if(show.confusion == T){
       if(is.null(cutoff)){
-         stop('Please specify cutoff for the confusion matrix')
+         message('No cutoff specified. Defaulting to 0.5 for producing the confusion matrix')
+         cutoff <- 0.5
       }
-      m <- confusionMatrix(probs.predicted, logicals.expected, cutoff = cutoff)
-      m <- rbind(
-         m[c('tp','fp')],
-         m[c('fn','tn')]
-      )
+      
+      if(!is.null(logicals.expected)){
+         m <- confusionMatrix(probs.predicted, logicals.expected, cutoff = cutoff)
+         m <- rbind(
+            m[c('tp','fp')],
+            m[c('fn','tn')]
+         )
+         cols <- matrix(c('red','grey50'), nrow(m), ncol(m), byrow = T)
+      } else {
+         m <- matrix( c(sum(probs.predicted >= cutoff), sum(probs.predicted < cutoff)), nrow = 2)
+         cols <- 'grey50'
+      }
       
       rownames(m) <- NULL
       colnames(m) <- NULL
       
-      cols <- matrix(c('red','grey50'), nrow(m), ncol(m), byrow = T)
       tt <- ttheme_minimal(core=list(fg_params = list(col = cols)))
       
       table_confusion <- tableGrob(m, theme = tt)
       table_xpos <- nrow(df) * 0.15
-
+      
+      #plot + annotation_custom(table_confusion, xmin=table_xpos, xmax=table_xpos, ymin=cutoff, ymax=cutoff)
       plot <- plot + annotation_custom(table_confusion, xmin=table_xpos, xmax=table_xpos, ymin=cutoff, ymax=cutoff)
+      
    }
+   
+   if( show.hline ){ plot <- plot + geom_hline(yintercept = cutoff, linetype = 2) }
    
    return(plot)
 }
